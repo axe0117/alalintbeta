70e5cfeb3b781daa85bdc779fd53da064887fb6c
axe0117 <axe0117@gmail.com>
2025-12-29 18:22:21 -0500

    forms

--- app/slides/page.tsx
+++ app/slides/page.tsx
@@ -1,7 +1,9 @@
 import {
   Flex,
   Image,
-  Text
+  Text,
+  Button,
+  Link
 } from "@chakra-ui/react";
 
 export default function Slides() {
@@ -67,6 +69,44 @@ export default function Slides() {
       <Image src="/Slide19.PNG/">
 
       </Image>
+
+      <Link href="https://form.jotform.com/253626865726065">
+          <Button
+            boxShadow="0 0 12px rgba(255,255,255,0.6)"
+            color="rgb(12, 60, 96)"
+            backgroundColor="white"
+            borderRadius="none"
+            fontSize="md"
+            fontWeight="bold"
+            size={{ base: "md", md: "lg" }}
+            _hover={{
+              backgroundColor: "rgb(12, 60, 96)",
+              color: "white",
+              transition: "0.3s"
+            }}
+          >
+            Attendee Registration
+          </Button>
+          </Link>
+
+          <Link href="https://form.jotform.com/253626561802154">
+          <Button
+            boxShadow="0 0 12px rgba(255,255,255,0.6)"
+            color="rgb(12, 60, 96)"
+            backgroundColor="white"
+            borderRadius="none"
+            fontSize="md"
+            fontWeight="bold"
+            size={{ base: "md", md: "lg" }}
+            _hover={{
+              backgroundColor: "rgb(12, 60, 96)",
+              color: "white",
+              transition: "0.3s"
+            }}
+          >
+            Exhibitor Application
+          </Button>
+          </Link>
     </Flex>
   );
 }
